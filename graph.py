cc81df7d0fb497bdf5bbf59c4b207bdf16a8b898
Quentindh <quentindehaes@outlook.com>
2020-08-20 16:17:08 +0200

    file reformatting

--- graph.py
+++ graph.py
@@ -1,5 +1,6 @@
 from Node import *
 
+
 class Graph:
     def __init__(self, start_node: Node):
         """
@@ -35,7 +36,6 @@ class Graph:
         """
         self.nodes = set(nodes)
 
-
     def to_dot(self, filename):
         """
         make a visual representation of the graph
@@ -50,7 +50,7 @@ class Graph:
         to_visit = [self.start_node]
 
         f.write("digraph G {\n")
-        f.write("  "+self.start_node.dot_value()+'\n')
+        f.write("  " + self.start_node.dot_value() + '\n')
         while len(to_visit) != 0:
             current = to_visit[0]
             to_visit = to_visit[1:]
@@ -59,7 +59,7 @@ class Graph:
             for node, amount in current.get_edges():
                 if node not in already_added:
                     already_added.add(node)
-                    f.write("  "+node.dot_value()+'\n')
+                    f.write("  " + node.dot_value() + '\n')
                 f.write("    s{} -> s{} [label = {}]\n".format(current.id, node.id, amount))
                 if node not in already_visited:
                     to_visit.append(node)
@@ -78,17 +78,16 @@ class Graph:
 
     def bellman_ford_alg(self):
         """
-        excecute the bellman ford algorithm to get shortest path of nodes (will be used as base to find positive cycles
+        execute the bellman ford algorithm to get shortest path of nodes (will be used as base to find positive cycles
         :return:
         """
         self.start_node.distance = 0
         # bellman ford needs V-1 iterations to certainly have shortest path (without negative cycles)
-        for i in range(len(self.nodes)-1):
+        for i in range(len(self.nodes) - 1):
             # by going over all nodes and their outgoing edges, we go over all edges
             for node in self.nodes:
                 for edge in node.edges:
                     edge[0].update_distance(node.distance + edge[1])
-
 
     def get_cycles(self):
         """
@@ -96,7 +95,7 @@ class Graph:
         all values on edges
         :return: a list of all nodes in cycles and their incrementation
         - the initial bellman part is O(VE)
-        - the next part is only a single bellamn run so O(E)
+        - the next part is only a single bellman run so O(E)
         - and the final part is V-1 times O(V³E) so in total O(V⁴E)
         """
         # TODO check possible issue with node being in 2 different cycles
@@ -146,7 +145,7 @@ class Graph:
         # the largest cycle will (in worst case ) have every node in the cycle, so we'll need to check for V times
         # to ensure we have checked for every cycle, since we've already done it once to ensure we have cycles we'll do
         # it V-1 times more
-        for i in range(len(self.nodes)-1):
+        for i in range(len(self.nodes) - 1):
 
             # update distances from previous run
             # O(E)
@@ -160,7 +159,6 @@ class Graph:
                     # if ,after already doing entire bellman ford, there still is a change in the distance,
                     # there exists a negative cycle, we will add this new node to the list
                     if edge[0].distance > node.distance - edge[1]:
-
                         edges_in_cycles.add((node, edge[0], edge[1]))
 
             # we check whether the edge can be added to our prospective cycles
@@ -170,18 +168,18 @@ class Graph:
                 for cycle in possible_cycles:
                     # because we know the maximum size of a cycle is V and the way this is constructed
                     # and possible_cycles can only have V! entries, which falls under O(V²)
-                    # making these forloops O(EV³)
+                    # making these for-loops O(EV³)
                     if edge[0] == cycle[0][-1]:
                         # if the newest node is our first node, the cycle is complete
                         if cycle[0][0] == edge[1]:
                             # the cycle is complete so we add it to complete_cycles
-                            complete_cycles.add((cycle[0]+(edge[1],), cycle[1]+ edge[2]))
+                            complete_cycles.add((cycle[0] + (edge[1],), cycle[1] + edge[2]))
                         elif edge[1] not in cycle[0]:
                             # the cycle isn't finished so we add it to the new possible cycles,
                             # if a subsidiary cycle is found, that cycle will also be found elsewhere
                             # and we no longer need this part
 
-                            new_possible_cycles.add((cycle[0]+(edge[1],), cycle[1] + edge[2]))
+                            new_possible_cycles.add((cycle[0] + (edge[1],), cycle[1] + edge[2]))
             possible_cycles = new_possible_cycles
 
         return complete_cycles
