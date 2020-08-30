734a217b991ee42532f865b92a565b358558fdad
Quentindh <quentindehaes@outlook.com>
2020-08-30 15:04:11 +0200

    added the calculation of the minimal value to be allowed to take a cycle

--- helper_functions.py
+++ helper_functions.py
@@ -41,13 +41,19 @@ def set_non_allowable_values(complete_cycles):
         for node_i in range(len(cycle[0]) - 1):
             not_allowed = []
             current_add = 0
+            minimal_add = 0
 
             not_allowed += [num - cycle[1] for num in cycle[0][:-1][node_i].get_disequalities()]
             current_add += distances[node_i % (len(cycle[0]) - 1)]
+            if current_add < minimal_add:
+                minimal_add = current_add
             # we run over every edge in the cycle (also O(V))
             for j in range(1, len(distances)):
                 not_allowed += [num - current_add for num in
                                 cycle[0][:-1][(node_i + j) % (len(cycle[0]) - 1)].get_disequalities()]
 
                 current_add += distances[(node_i + j) % (len(cycle[0]) - 1)]
+                if current_add < minimal_add:
+                    minimal_add = current_add
             cycle[0][node_i].non_cyclables = not_allowed
+            cycle[0][node_i].minimal_cyclable = -minimal_add
